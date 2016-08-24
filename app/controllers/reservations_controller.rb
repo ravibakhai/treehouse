de15965fb7e574b9144f6ff1a159d9322381f49e
Ravi Bakhai <ravi.bakhai@gmail.com>
2016-08-24 12:46:55 -0400

    end date and price calculator

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,21 +2,39 @@ class ReservationsController < ApplicationController
   before_action :authenticate_user!
 
   def preload
-  property = Property.find(params[:property_id])
-  today = Date.today
-  reservations = property.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+		property = Property.find(params[:property_id])
+		today = Date.today
+		reservations = property.reservations.where("start_date >= ? OR end_date >= ?", today, today)
 
-  render json: reservations
-  end
+		render json: reservations
+	end
 
-  def create
-  @reservation = current_user.reservations.create(reservation_params)
+	def preview
+		start_date = Date.parse(params[:start_date])
+		end_date = Date.parse(params[:end_date])
 
-  redirect_to @reservation.property, notice: "Your sublet request has been made..."
-  end
+		output = {
+			conflict: is_conflict(start_date, end_date)
+		}
 
-  private
-    def reservation_params
-    params.require(:reservation).permit(:start_date, :end_date, :price, :total, :property_id)
-    end
-  end
+		render json: output
+	end
+
+	def create
+		@reservation = current_user.reservations.create(reservation_params)
+
+		redirect_to @reservation.property, notice: "Your reservation has been created..."
+	end
+
+	private
+		def is_conflict(start_date, end_date)
+			property = Property.find(params[:property_id])
+
+			check = property.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+			check.size > 0? true : false
+		end
+
+		def reservation_params
+			params.require(:reservation).permit(:start_date, :end_date, :price, :total, :property_id)
+		end
+end
