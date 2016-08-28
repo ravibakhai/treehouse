1f5822a1e1aaddee89f7ee21990bc8e4a496bc84
Ravi Bakhai <ravi.bakhai@gmail.com>
2016-08-28 14:38:08 -0400

    fixed a few bugs

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,7 +2,7 @@ class Property < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   has_many :reservations
-  
+
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
@@ -14,5 +14,13 @@ class Property < ActiveRecord::Base
   validates :listing_name, presence: true, length: {maximum: 50}
   validates :summary, presence: true, length: {maximum: 500}
   validates :address, presence: true
+
+  def show_first_photo(size)
+    if self.photos.length == 0
+      "treehouselogo.png"
+    else
+      self.photos[0].image.url(size)
+    end
+  end
 
 end
