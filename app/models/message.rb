1a0d3219d5ddaa04e02bd64724c8f4e709333c26
Ravi Bakhai <ravi.bakhai@gmail.com>
2016-08-29 19:22:55 -0400

    message

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,7 @@ class Message < ActiveRecord::Base
   validates_presence_of :content, :conversation_id, :user_id
 
   def message_time
-    created_at.strftime("%v")
+  	created_at.strftime("%v")
   end
+
 end
