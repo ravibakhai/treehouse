1f5d2673274e6a14270f7799a4cced65314694c7
Ravi Bakhai <ravi.bakhai@gmail.com>
2016-12-26 12:44:21 -0500

    finished task 14

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,6 @@ class PagesController < ApplicationController
 
     if params[:search].present? && params[:search].strip != ""
       session[:loc_search] = params[:search]
-    else
-      session[:loc_search] = ""
     end
 
     arrResult = Array.new
@@ -24,6 +22,26 @@ class PagesController < ApplicationController
 
     @arrProperties = @properties.to_a
 
-    if (params[:start_date] && params[:end_date])
+    if (params[:start_date] && params[:end_date] && !params[:start_date].empty? & !params[:end_date].empty?)
+
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+
+      @properties.each do |property|
+
+        not_available = property.reservations.where(
+          "(? <= start_date AND start_date <= ?)
+          OR (? <= end_date AND end_date <= ?)
+          OR (start_date < ? AND ? < end_date)",
+          start_date, end_date,
+          start_date, end_date,
+          start_date, end_date
+        ).limit(1)
+
+        if not_available.length > 0
+          @arrProperties.delete(property)
+        end
+      end
   end
+end
 end
