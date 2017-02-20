68888530c113d00e7e8cca83aa131c5c5876b030
Ravi Bakhai <ravi.bakhai@gmail.com>
2017-02-20 13:50:10 -0600

    adf

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'ravi0613',
+  :password => 'jq4smrue',
+  :domain => 'treehousebeta.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,16 +38,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    domain: 'treehousebeta.herokuapp.com',
-    port: 587,
-    enable_starttls_auto: true,
-    user_name: 'ravi0613',
-    password: 'jq4smrue',
-    authentication: 'plain'
-}
+#   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+#
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {
+#     address: 'smtp.sendgrid.net',
+#     domain: 'treehousebeta.herokuapp.com',
+#     port: 587,
+#     enable_starttls_auto: true,
+#     user_name: 'ravi0613',
+#     password: 'jq4smrue',
+#     authentication: 'plain'
+# }
 end
