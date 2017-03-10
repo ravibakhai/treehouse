e41f0c6745bd2c8abdeb18344f2943dac73526de
Ravi Bakhai <ravi.bakhai@gmail.com>
2017-03-10 09:19:28 -0600

    adf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,15 +38,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
 #   config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-#
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    enable_starttls_auto: true,
-    user_name: 'ravi1996',
-    password: 'marshmello1996',
-    authentication: 'plain'
-}
+#   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+# #
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {
+#     address: 'smtp.sendgrid.net',
+#     port: 587,
+#     enable_starttls_auto: true,
+#     user_name: 'ravi1996',
+#     password: 'marshmello1996',
+#     authentication: 'plain'
+# }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,14 @@ Rails.application.configure do
       s3_region: ENV.fetch('AWS_REGION'),
     }
   }
-  # config.action_mailer.default_url_options = { :host => 'treehousebeta.herokuapp.com'}
-  # ActionMailer::Base.smtp_settings = {
-  #   :user_name => 'ravi0613',
-  #   :password => 'jq4smrue',
-  #   :domain => 'treehousebeta.herokuapp.com',
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
+  config.action_mailer.default_url_options = { :host => 'treehousebeta.herokuapp.com'}
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
