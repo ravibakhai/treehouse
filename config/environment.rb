3d4955ec8ec37ee58be226aa5e0fa2f759e4fcda
Ravi Bakhai <ravi.bakhai@gmail.com>
2017-03-12 18:30:37 -0500

    adf

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'ravi0613',
-  :password => 'jq4smrue',
-  :domain => 'treehousebeta.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'ravi0613',
+#   :password => 'jq4smrue',
+#   :domain => 'treehousebeta.herokuapp.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
