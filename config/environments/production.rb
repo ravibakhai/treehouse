867fbd608a009ae328e7f057698981925eeb62d6
Ravi Bakhai <ravi.bakhai@gmail.com>
2017-03-27 20:57:02 -0500

    Adfd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
       s3_region: ENV.fetch('AWS_REGION'),
     }
   }
-  config.action_mailer.default_url_options = { :host => 'treehousebeta.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'wwww.usetreehouse.com'}
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
